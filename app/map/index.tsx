951b9c9d939ade6dc591bf4bbfa693772b896d86
Mauri <mauri.monzon91@gmail.com>
2024-11-27 10:06:50 -0300

    Creado huella digital sha-1

--- app/map/index.tsx
+++ app/map/index.tsx
@@ -1,12 +1,32 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import React from "react";
+import MapView from "react-native-maps";
 
 const MapsScreen = () => {
   return (
-    <View>
-      <Text>MapsScreen</Text>
+    <View style={styles.container}>
+      <MapView
+        style={styles.map}
+        // provider="google"
+        initialRegion={{
+          latitude: -34.688772,
+          longitude: -58.566762,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      />
     </View>
   );
 };
 
 export default MapsScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    width: "100%",
+    height: "100%",
+  },
+});
