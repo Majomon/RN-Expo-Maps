77e9fe58d2b2daf3aeaee5c3d0ce8de3609d3656
Mauri <mauri.monzon91@gmail.com>
2024-11-28 16:08:35 -0300

    ,Accioon para seguir y perdir ubicacion

---
+++ core/actions/location/location.ts
@@ -0,0 +1,36 @@
+import { LatLng } from "@/infraestructure/interfaces/lat-lng";
+import * as Location from "expo-location";
+
+export const getCurrentLocation = async (): Promise<LatLng> => {
+  try {
+    const { coords } = await Location.getCurrentPositionAsync({
+      accuracy: Location.Accuracy.Highest,
+    });
+
+    return {
+      latitude: coords.latitude,
+      longitude: coords.longitude,
+    };
+  } catch (error) {
+    throw new Error(" Error al obtener la ubicación");
+  }
+};
+
+export const wathCurrentPosition = (
+  locationCallback: (location: LatLng) => void
+) => {
+  return Location.watchPositionAsync(
+    {
+      accuracy: Location.Accuracy.Highest,
+      timeInterval: 1000,
+      //Esto es en mts entiendo yo
+      distanceInterval: 10,
+    },
+    ({ coords }) => {
+      locationCallback({
+        latitude: coords.latitude,
+        longitude: coords.longitude,
+      });
+    }
+  );
+};
